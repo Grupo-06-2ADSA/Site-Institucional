2b7b6d40a30f3e67e7e6c1bc93e34bfa7a1c6616
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-04-18 12:23:52 -0300

    Adicionando API web-data-viz

---
+++ site/public/js/sessao.js
@@ -0,0 +1,40 @@
+// sessão
+function validarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var cnpj = sessionStorage.EMPRESA_USUARIO;
+    var tipo = sessionStorage.TIPO_USUARIO;
+
+    var nomeUsuario = document.getElementById("nomeUsuario");
+    var tipoUsuario = document.getElementById("tipoUsuario");
+    
+    if (email != null && nome != null && tipo != null && cnpj != null) {
+        nomeUsuario.innerHTML = nome;
+        tipoUsuario.innerHTML = tipo
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.style.display = "flex";
+        divErrosLogin.innerHTML = texto;
+    }
+}
+
